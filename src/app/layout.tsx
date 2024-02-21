e367ff602bd9ffba1b0314e6d86ee30f2fa10bcf
Nabin Hamal <nabinhamal2@gmail.com>
2024-02-21 17:13:34 +0545

    edited

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import {ClerkProvider} from "@clerk/nextjs"
-import {dark} from "@clerk/themes"
+
+import { ClerkProvider } from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
 
 
 const inter = Inter({ subsets: ["latin"] });
